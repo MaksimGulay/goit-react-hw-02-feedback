495bd471bf66386ac947798c1315f0ff86dc041e
Maksim Gulay <maksimgulaj@gmail.com>
2023-08-14 21:42:41 +0300

    Practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import { type } from '@testing-library/user-event/dist/type';
+import { Component } from 'react';
+// import { StateExample } from '../components/StateExample/StateExample';
+
+// export const App = () => {
+//   return (
+//     <div>
+//       <StateExample />
+//     </div>
+//   );
+// };
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleX = type => {
+    this.setState(prevState => {
+      return {
+        [type]: prevState[type] + 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.handleX('good')}>
+          {this.state.good}Good
+        </button>
+        <button onClick={() => this.handleX('bad')}>Bad</button>
+        <button onClick={() => this.handleX('neutral')}>Neutral</button>
+      </div>
+    );
+  }
+}
